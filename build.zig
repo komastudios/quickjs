ca4bf36f57e95f3d7e55c73ddbd00a25b523c180
Urs Hanselmann <git@hanselmann.io>
2023-06-28 15:11:55 +0200

    add iOS target support

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const fs = std.fs;
 
 const quickjs_sources = [_][]const u8{
     "cutils.c",
@@ -18,6 +19,10 @@ const quickjs_cflags_android: []const []const u8 = quickjs_cflags ++ [_][]const 
     "-fno-builtin-bcmp",
 };
 
+const quickjs_cflags_ios: []const []const u8 = quickjs_cflags ++ [_][]const u8{
+    //"-fno-builtin-bcmp",
+};
+
 const quickjs_public_headers = [_][]const u8{ "quickjs.h", "quickjs-libc.h" };
 
 pub fn build(b: *std.Build) !void {
@@ -33,6 +38,8 @@ pub fn build(b: *std.Build) !void {
     });
     lib_static.addIncludePath(".");
 
+    const build_shared_lib = lib_static.target_info.target.os.tag != .ios;
+
     switch (lib_static.target_info.target.os.tag) {
         .windows => {
             lib_static.defineCMacro("QUICKJS_DLL", null);
@@ -45,6 +52,14 @@ pub fn build(b: *std.Build) !void {
     if (android) {
         lib_static.defineCMacro("__ANDROID__", null);
         lib_static.addCSourceFiles(&quickjs_sources, quickjs_cflags_android);
+    } else if (lib_static.target_info.target.os.tag == .ios) {
+        if (b.sysroot) |sysroot| {
+            lib_static.addIncludePath(try fs.path.join(b.allocator, &.{ sysroot, "usr/include" }));
+        } else {
+            std.log.warn("Missing sysroot", .{});
+        }
+
+        lib_static.addCSourceFiles(&quickjs_sources, quickjs_cflags_ios);
     } else {
         lib_static.addCSourceFiles(&quickjs_sources, quickjs_cflags);
     }
@@ -53,47 +68,49 @@ pub fn build(b: *std.Build) !void {
     b.installArtifact(lib_static);
     for (quickjs_public_headers) |h| lib_static.installHeader(h, h);
 
-    const lib_shared = b.addSharedLibrary(.{
-        .name = "quickjs",
-        .target = target,
-        .optimize = optimize,
-    });
-    lib_shared.addIncludePath(".");
+    if (build_shared_lib) {
+        const lib_shared = b.addSharedLibrary(.{
+            .name = "quickjs",
+            .target = target,
+            .optimize = optimize,
+        });
+        lib_shared.addIncludePath(".");
 
-    switch (lib_shared.target_info.target.os.tag) {
-        .windows => {
-            lib_shared.defineCMacro("QUICKJS_DLL", null);
-            lib_shared.defineCMacro("JS_STRICT_NAN_BOXING", null);
-        },
-        else => {
-            lib_shared.defineCMacro("_GNU_SOURCE", null);
-        },
+        switch (lib_shared.target_info.target.os.tag) {
+            .windows => {
+                lib_shared.defineCMacro("QUICKJS_DLL", null);
+                lib_shared.defineCMacro("JS_STRICT_NAN_BOXING", null);
+            },
+            else => {
+                lib_shared.defineCMacro("_GNU_SOURCE", null);
+            },
+        }
+        if (android) {
+            lib_shared.defineCMacro("__ANDROID__", null);
+            lib_shared.addCSourceFiles(&quickjs_sources, quickjs_cflags_android);
+        } else {
+            lib_shared.addCSourceFiles(&quickjs_sources, quickjs_cflags);
+        }
+        lib_shared.linkLibC();
+
+        b.installArtifact(lib_shared);
+
+        const test_extern = b.addExecutable(.{
+            .name = "test-extern",
+            .target = target,
+            .optimize = optimize,
+        });
+        test_extern.addCSourceFiles(&.{"test-extern.c"}, &.{});
+        test_extern.linkLibrary(lib_shared);
+        test_extern.defineCMacro("QUICKJS_DLL", null);
+        b.installArtifact(test_extern);
+
+        const run_cmd = b.addRunArtifact(test_extern);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        const run_step = b.step("test", "Run tests");
+        run_step.dependOn(&run_cmd.step);
     }
-    if (android) {
-        lib_shared.defineCMacro("__ANDROID__", null);
-        lib_shared.addCSourceFiles(&quickjs_sources, quickjs_cflags_android);
-    } else {
-        lib_shared.addCSourceFiles(&quickjs_sources, quickjs_cflags);
-    }
-    lib_shared.linkLibC();
-
-    b.installArtifact(lib_shared);
-
-    const test_extern = b.addExecutable(.{
-        .name = "test-extern",
-        .target = target,
-        .optimize = optimize,
-    });
-    test_extern.addCSourceFiles(&.{"test-extern.c"}, &.{});
-    test_extern.linkLibrary(lib_shared);
-    test_extern.defineCMacro("QUICKJS_DLL", null);
-    b.installArtifact(test_extern);
-
-    const run_cmd = b.addRunArtifact(test_extern);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("test", "Run tests");
-    run_step.dependOn(&run_cmd.step);
 }
