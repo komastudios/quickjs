fd71b734bd4f1cb5357bc7346f6de38217c15996
Urs Hanselmann <git@hanselmann.io>
2023-07-02 14:53:57 +0200

    add CMake project file

--- qjs.c
+++ qjs.c
@@ -454,9 +454,10 @@ int main(int argc, char **argv)
         }
     }
 
+#ifdef CONFIG_BIGNUM
     if (load_jscalc)
         bignum_ext = 1;
-
+#endif
     if (trace_memory) {
         js_trace_malloc_init(&trace_data);
         rt = JS_NewRuntime2(&trace_mf, &trace_data);
